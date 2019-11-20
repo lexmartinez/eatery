0fd3c1fd55d4e9f53c558ee6eda3324ab9eea33b
Lex Martinez <lex0316@gmail.com>
2019-11-20 00:07:17 -0500

    results component

--- src/screens/Results/view.tsx
+++ src/screens/Results/view.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
-import { ScrollView, View } from 'react-native';
+import { ScrollView, View, Text } from 'react-native';
 import { Code } from 'react-content-loader/native';
 import { Actions } from 'react-native-router-flux';
 import I18n from '../../config/i18n';
-import TextInput from '../../components/TextInput';
-import { labelize } from '../../config/Utilities';
 import styles from './style';
 
 export default class Results extends React.Component<ResultsProps> {
@@ -22,10 +20,16 @@ export default class Results extends React.Component<ResultsProps> {
     }
     
     render() {
-        const { loading } = { ...this.props };
+        const { loading, results } = { ...this.props };
         return !loading ? (
             <ScrollView style={styles.main} showsVerticalScrollIndicator={false}>
-                
+                { (results || []).map((restaurant) => {
+                    const { name = '', address = '', image_url = ''} = { ...restaurant };
+                    return <View>
+                        <Text>{name}</Text>
+                        <Text>{address}</Text>
+                    </View>
+                })};
             </ScrollView>
         ) : (
             <View style={styles.mainEmpty}>
